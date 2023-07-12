1d40c40d8b8df6dac6cc1971b25f6db4fd4a8f47
cabbage16 <baejaehyeon0106@gmail.com>
2023-07-12 15:56:43 +0900

    임시 저장

---
+++ javascript/search.js
@@ -0,0 +1,37 @@
+var cities = [
+    {
+        id: 1,
+        name: "바르셀로나",
+        url: './img/City/Barcelona.jpg'
+    },
+    {
+        id: 2,
+        name: "하와이",
+        url: './img/City/Hawaii.jpg'
+    },
+    {
+        id: 3,
+        name: "런던",
+        url: './img/City/London.jpg'
+    },
+    {
+        id: 4,
+        name: "마이애미",
+        url: './img/City/Miami.jpg'
+    },
+    {
+        id: 5,
+        name: "파리",
+        url: './img/City/Paris.jpg'
+    },
+    {
+        id: 6,
+        name: "로마",
+        url: './img/City/Rome.jpg'
+    },
+    {
+        id: 7,
+        name: "베니스",
+        url: './img/City/Venis.jpg'
+    },
+]
